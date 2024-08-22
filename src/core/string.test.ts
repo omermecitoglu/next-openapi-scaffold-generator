0d40cbb393c261d3f8e798c5ecb6f0d8534c6ae6
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-08-22 15:33:42 +0700

    add string utils

---
+++ src/core/string.test.ts
@@ -0,0 +1,41 @@
+import { expect, test } from "@jest/globals";
+import { convertToCamelCase, convertToCapitalCase, convertToKebabCase, convertToNoCase, convertToPascalCase } from "./string";
+
+test("convertToCamelCase", () => {
+  expect(convertToCamelCase("user record", false)).toBe("userRecord");
+  expect(convertToCamelCase("user record", true)).toBe("userRecords");
+  expect(convertToCamelCase("user entry", false)).toBe("userEntry");
+  expect(convertToCamelCase("user entry", true)).toBe("userEntries");
+});
+
+test("convertToKebabCase", () => {
+  expect(convertToKebabCase("user record", false)).toBe("user-record");
+  expect(convertToKebabCase("user record", true)).toBe("user-records");
+  expect(convertToKebabCase("user entry", false)).toBe("user-entry");
+  expect(convertToKebabCase("user entry", true)).toBe("user-entries");
+});
+
+test("convertToPascalCase", () => {
+  expect(convertToPascalCase("user record", false)).toBe("UserRecord");
+  expect(convertToPascalCase("user record", true)).toBe("UserRecords");
+  expect(convertToPascalCase("user entry", false)).toBe("UserEntry");
+  expect(convertToPascalCase("user entry", true)).toBe("UserEntries");
+});
+
+test("convertToNoCase", () => {
+  expect(convertToNoCase("user record", false)).toBe("user record");
+  expect(convertToNoCase("user record", true)).toBe("user records");
+  expect(convertToNoCase("user entry", false)).toBe("user entry");
+  expect(convertToNoCase("user entry", true)).toBe("user entries");
+});
+
+test("convertToCapitalCase", () => {
+  expect(convertToCapitalCase("user record", false, false)).toBe("User Record");
+  expect(convertToCapitalCase("user record", false, true)).toBe("User record");
+  expect(convertToCapitalCase("user record", true, false)).toBe("User Records");
+  expect(convertToCapitalCase("user record", true, true)).toBe("User records");
+  expect(convertToCapitalCase("user entry", false, false)).toBe("User Entry");
+  expect(convertToCapitalCase("user entry", false, true)).toBe("User entry");
+  expect(convertToCapitalCase("user entry", true, false)).toBe("User Entries");
+  expect(convertToCapitalCase("user entry", true, true)).toBe("User entries");
+});
---
+++ src/core/string.ts
@@ -0,0 +1,35 @@
+import { camelCase, capitalCase, kebabCase, noCase, pascalCase } from "change-case";
+import pluralize from "pluralize";
+
+function capitalize(text: string) {
+  if (text.length === 0) return text;
+  return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+}
+
+export function convertToCamelCase(text: string, plural: boolean) {
+  const properText = plural ? pluralize(text) : pluralize.singular(text);
+  return camelCase(properText);
+}
+
+export function convertToKebabCase(text: string, plural: boolean) {
+  const properText = plural ? pluralize(text) : pluralize.singular(text);
+  return kebabCase(properText);
+}
+
+export function convertToPascalCase(text: string, plural: boolean) {
+  const properText = plural ? pluralize(text) : pluralize.singular(text);
+  return pascalCase(properText);
+}
+
+export function convertToNoCase(text: string, plural: boolean) {
+  const properText = plural ? pluralize(text) : pluralize.singular(text);
+  return noCase(properText);
+}
+
+export function convertToCapitalCase(text: string, plural: boolean, onlyFirst: boolean) {
+  const properText = plural ? pluralize(text) : pluralize.singular(text);
+  if (onlyFirst) {
+    return capitalize(noCase(properText));
+  }
+  return capitalCase(properText);
+}
