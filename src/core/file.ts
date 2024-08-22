59a92f118f72528067315576a1c62c3d3262099e
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-08-22 16:06:01 +0700

    add save file util function

---
+++ src/core/file.ts
@@ -0,0 +1,9 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
+export async function saveFile(directory: string, fileName: string, content: string) {
+  const dirPath = path.resolve(process.cwd(), directory);
+  await fs.mkdir(dirPath, { recursive: true });
+  const filePath = path.resolve(dirPath, fileName);
+  await fs.writeFile(filePath, content, "utf8");
+}
