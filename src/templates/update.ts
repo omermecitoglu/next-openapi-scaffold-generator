aaef9a1a8a228a0d34f575fc6e6a74053afcf771
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-08-22 15:34:40 +0700

    add update operation template

---
+++ src/templates/update.ts
@@ -0,0 +1,22 @@
+import { convertToCamelCase, convertToCapitalCase, convertToNoCase, convertToPascalCase } from "~/core/string";
+import getTemplate from "~/core/template";
+import handleBarsTemplate from "./update.hbs";
+
+type UpdateOperationTemplate = {
+  camelCaseSingular: string,
+  capitalCasePlural: string,
+  noCaseSingular: string,
+  onlyFirstCapitalCaseSingular: string,
+  pascalCaseSingular: string,
+};
+
+export default function generateUpdateOperation(modelName: string) {
+  const template = getTemplate<UpdateOperationTemplate>(handleBarsTemplate);
+  return template({
+    camelCaseSingular: convertToCamelCase(modelName, false),
+    capitalCasePlural: convertToCapitalCase(modelName, true, false),
+    noCaseSingular: convertToNoCase(modelName, false),
+    onlyFirstCapitalCaseSingular: convertToCapitalCase(modelName, false, true),
+    pascalCaseSingular: convertToPascalCase(modelName, false),
+  });
+}
