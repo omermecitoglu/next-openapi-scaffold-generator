f4b5c92507b506c9636184e1aced177af31fb372
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-08-22 16:20:23 +0700

    feat: add api routes generator

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import { saveFile } from "./core/file";
+import { convertToKebabCase } from "./core/string";
 import { askQuestions } from "./prompt";
+import generateApiRoute from "./templates/api-route";
+import generateApiRouteWithId from "./templates/api-route-with-id";
 
-(async () => {
-  const [modelName] = await askQuestions(["Enter Model Name: "]);
-  console.log(modelName);
-})();
+const [modelName] = await askQuestions(["Enter Model Name: "]);
+await saveFile(`src/app/${convertToKebabCase(modelName, true)}`, "route.ts", generateApiRoute(modelName));
+await saveFile(`src/app/${convertToKebabCase(modelName, true)}/[id]`, "route.ts", generateApiRouteWithId(modelName));
