c6802d5bfbab61ff5db01063bfd075bcac600449
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-08-23 11:39:29 +0700

    feat: create operation test generator

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { askQuestions } from "./prompt";
 import generateApiRoute from "./templates/api-route";
 import generateApiRouteWithId from "./templates/api-route-with-id";
 import generateModel from "./templates/model";
+import generateCreateTest from "./templates/operation-tests/create";
 import generateReadAllTest from "./templates/operation-tests/read-all";
 import generateCreateOperation from "./templates/operations/create";
 import generateReadAllOperation from "./templates/operations/read-all";
@@ -17,3 +18,4 @@ await saveFile("src/models", `${convertToKebabCase(modelName, false)}.ts`, gener
 await saveFile("src/operations", `get${convertToPascalCase(modelName, true)}.ts`, generateReadAllOperation(modelName));
 await saveFile("src/operations", `get${convertToPascalCase(modelName, true)}.test.ts`, generateReadAllTest(modelName));
 await saveFile("src/operations", `create${convertToPascalCase(modelName, false)}.ts`, generateCreateOperation(modelName));
+await saveFile("src/operations", `create${convertToPascalCase(modelName, false)}.test.ts`, generateCreateTest(modelName));
---
+++ src/templates/operation-tests/create.ts
@@ -0,0 +1,32 @@
+import { convertToCamelCase, convertToKebabCase, convertToNoCase, convertToPascalCase, convertToSnakeCase } from "~/core/string";
+import getTemplate from "~/core/template";
+import handleBarsTemplate from "./read-all.hbs";
+
+type CreateTestTemplate = {
+  camelCasePlural: string,
+  camelCaseSingular: string,
+  kebabCasePlural: string,
+  kebabCaseSingular: string,
+  noCasePlural: string,
+  noCaseSingular: string,
+  pascalCasePlural: string,
+  pascalCaseSingular: string,
+  snakeCasePlural: string,
+  snakeCaseSingular: string,
+};
+
+export default function generateCreateTest(modelName: string) {
+  const template = getTemplate<CreateTestTemplate>(handleBarsTemplate);
+  return template({
+    camelCasePlural: convertToCamelCase(modelName, true),
+    camelCaseSingular: convertToCamelCase(modelName, false),
+    kebabCasePlural: convertToKebabCase(modelName, true),
+    kebabCaseSingular: convertToKebabCase(modelName, false),
+    noCasePlural: convertToNoCase(modelName, true),
+    noCaseSingular: convertToNoCase(modelName, false),
+    pascalCasePlural: convertToPascalCase(modelName, true),
+    pascalCaseSingular: convertToPascalCase(modelName, false),
+    snakeCasePlural: convertToSnakeCase(modelName, true),
+    snakeCaseSingular: convertToSnakeCase(modelName, false),
+  });
+}
