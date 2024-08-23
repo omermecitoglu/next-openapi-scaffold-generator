20a358f7aa07c7d399b3bb9f9d880edda865d9c0
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-08-23 08:18:29 +0700

    build: remove commonjs output

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   outDir: "bin",
   entry: ["src/index.ts"],
-  format: ["cjs", "esm"],
+  format: ["esm"],
   splitting: false,
   clean: true,
   external: ["change-case", "pluralize", "handlebars"],
