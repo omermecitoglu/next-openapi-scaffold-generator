f5e89997eb02124be0e58224c8c3e2556018a12a
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2025-12-13 12:29:37 +0700

    internal: checkFile utility function

--- src/core/file.ts
+++ src/core/file.ts
@@ -14,3 +14,17 @@ export async function appendFile(directory: string, fileName: string, content: s
   const filePath = path.resolve(dirPath, fileName);
   await fs.appendFile(filePath, content + "\n", "utf8");
 }
+
+export async function checkFile(directory: string, fileName: string, content: string) {
+  const dirPath = path.resolve(process.cwd(), directory);
+  const filePath = path.resolve(dirPath, fileName);
+  try {
+    const fileContent = await fs.readFile(filePath, "utf8");
+    return fileContent.includes(content);
+  } catch (err: unknown) {
+    if (err && typeof err === "object" && "code" in err && err.code === "ENOENT") {
+      return false;
+    }
+    throw err;
+  }
+}
