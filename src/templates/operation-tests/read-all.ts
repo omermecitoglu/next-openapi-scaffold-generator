bc54043c594ca926cab3f1bc64d5761794465464
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2026-01-17 17:12:58 +0700

    fix: alias name casing in operation tests

--- src/templates/operation-tests/read-all.ts
+++ src/templates/operation-tests/read-all.ts
@@ -3,6 +3,7 @@ import getTemplate from "~/core/template";
 import handleBarsTemplate from "./read-all.hbs";
 
 type ReadAllTestTemplate = {
+  kebabCasePlural: string,
   kebabCaseSingular: string,
   noCasePlural: string,
   pascalCasePlural: string,
@@ -11,6 +12,7 @@ type ReadAllTestTemplate = {
 
 export default function generateReadAllTest(modelName: string) {
   return getTemplate<ReadAllTestTemplate>(handleBarsTemplate)({
+    kebabCasePlural: kebabCase(modelName, true),
     kebabCaseSingular: kebabCase(modelName, false),
     noCasePlural: noCase(modelName, true),
     pascalCasePlural: pascalCase(modelName, true),
--- src/templates/operation-tests/read.ts
+++ src/templates/operation-tests/read.ts
@@ -3,6 +3,7 @@ import getTemplate from "~/core/template";
 import handleBarsTemplate from "./read.hbs";
 
 type ReadTestTemplate = {
+  kebabCasePlural: string,
   kebabCaseSingular: string,
   noCaseSingular: string,
   pascalCaseSingular: string,
@@ -11,6 +12,7 @@ type ReadTestTemplate = {
 
 export default function generateReadTest(modelName: string) {
   return getTemplate<ReadTestTemplate>(handleBarsTemplate)({
+    kebabCasePlural: kebabCase(modelName, true),
     kebabCaseSingular: kebabCase(modelName, false),
     noCaseSingular: noCase(modelName, false),
     pascalCaseSingular: pascalCase(modelName, false),
